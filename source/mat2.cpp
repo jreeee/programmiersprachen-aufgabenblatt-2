5aa0382d25e0dcf7d1fb9b23ab05eec51121b2ee
Jeremias Kilian <jeremias.michael.kilian@uni-weimar.de>
2020-05-25 00:22:42 +0200

    updated the * and *= implementation

--- source/mat2.cpp
+++ source/mat2.cpp
@@ -6,8 +6,12 @@ Mat2& Mat2::operator*=(Mat2 const& m) {
     float tmp_10 = e_10;
     float tmp_11 = e_11;
     e_00 = tmp_00 * m.e_00 + tmp_10 * m.e_01;
+    tmp_00 = e_00;
+    tmp_10 = e_10;
     e_10 = tmp_00 * m.e_10 + tmp_10 * m.e_11;
     e_01 = tmp_01 * m.e_00 + tmp_11 * m.e_01;
+    tmp_01 = e_01;
+    tmp_11 = e_11;
     e_11 = tmp_01 * m.e_10 + tmp_11 * m.e_11;
     return *this;
 }
--- source/mat2.hpp
+++ source/mat2.hpp
@@ -14,5 +14,6 @@ struct Mat2
     
     Mat2& operator *=(Mat2 const& m);
 };
+Mat2 operator*(Mat2 const& m1, Mat2 const& m2);
 
 #endif
