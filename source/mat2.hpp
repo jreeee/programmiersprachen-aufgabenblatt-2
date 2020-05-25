4c9dd9ac2ac102a77841f5aff4202d6674c4395d
Jeremias Kilian <jeremias.michael.kilian@uni-weimar.de>
2020-05-25 15:23:13 +0200

    fixed the 2.6 operators

--- source/mat2.hpp
+++ source/mat2.hpp
@@ -1,6 +1,7 @@
 #ifndef MAT2_HPP
 #define MAT2_HPP
 
+#include "vec2.hpp"
 #include <array>
 
 struct Mat2
@@ -16,7 +17,9 @@ struct Mat2
     float det() const;
 
 };
+
 Mat2 operator*(Mat2 const& m1, Mat2 const& m2);
+Vec2 operator*(Mat2 const& m, Vec2 const& v);
 Mat2 inverse(Mat2 const& m);
 Mat2 transpose(Mat2 const& m);
 Mat2 make_rotation_mat2(float phi);
