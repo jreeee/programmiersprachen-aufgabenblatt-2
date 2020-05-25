a1a28717c3ceefb22710cad338550ddd4a04fa9d
Jeremias Kilian <jeremias.michael.kilian@uni-weimar.de>
2020-05-25 13:00:22 +0200

    fixed operator *

--- source/mat2.cpp
+++ source/mat2.cpp
@@ -1,18 +1,14 @@
 #include "mat2.hpp"
 
 Mat2& Mat2::operator*=(Mat2 const& m) {
-    float tmp_00 = e_00;
-    float tmp_01 = e_01;
-    float tmp_10 = e_10;
-    float tmp_11 = e_11;
-    e_00 = tmp_00 * m.e_00 + tmp_10 * m.e_01;
-    tmp_00 = e_00;
-    tmp_10 = e_10;
-    e_10 = tmp_00 * m.e_10 + tmp_10 * m.e_11;
-    e_01 = tmp_01 * m.e_00 + tmp_11 * m.e_01;
-    tmp_01 = e_01;
-    tmp_11 = e_11;
-    e_11 = tmp_01 * m.e_10 + tmp_11 * m.e_11;
+    Mat2 a{*this};
+    Mat2 b{
+        a.e_00 * m.e_00 + a.e_10 * m.e_01,
+        a.e_00 * m.e_10 + a.e_10 * m.e_11,
+        a.e_01 * m.e_00 + a.e_11 * m.e_01,
+        a.e_01 * m.e_10 + a.e_11 * m.e_11 };
+
+    *this = b;
     return *this;
 }
 
