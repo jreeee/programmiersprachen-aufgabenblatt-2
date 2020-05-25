821af51bedf974a8d22cdc9a6d46dfb01c1d0540
Jeremias Kilian <jeremias.michael.kilian@uni-weimar.de>
2020-05-25 14:15:37 +0200

    implemented the 2.6 operators

--- source/mat2.cpp
+++ source/mat2.cpp
@@ -1,4 +1,6 @@
 #include "mat2.hpp"
+#include "vec2.hpp"
+#include <cmath>
 
 Mat2& Mat2::operator*=(Mat2 const& m) {
     Mat2 a{*this};
@@ -12,6 +14,28 @@ Mat2& Mat2::operator*=(Mat2 const& m) {
     return *this;
 }
 
+float Mat2::det() const {
+    return e_00 * e_11 - e_10 * e_01;
+}
+
+
 Mat2 operator*(Mat2 const& m1, Mat2 const& m2) {
     return Mat2(m1) *= m2;
+}
+Vec2 operator*(Mat2 const& m, Vec2 const& v) {
+    return {v.x * (m.e_00 + m.e_10), v.y * (m.e_01 + m.e_11)}; 
+}
+
+Mat2 inverse(Mat2 const& m) {
+    float det = m.det();
+    if (det == 0.0f) return m;
+    return {m.e_11 / det, -m.e_10 / det, -m.e_01 / det, m.e_00 / det};
+}
+
+Mat2 transpose(Mat2 const& m) {
+    return {m.e_00, m.e_01, m.e_10, m.e_11};
+}
+
+Mat2 make_rotation_mat2(float phi) {
+    return {cos (phi), -sin (phi), sin (phi), cos (phi)};
 }
--- source/mat2.hpp
+++ source/mat2.hpp
@@ -13,7 +13,12 @@ struct Mat2
     float e_11 = 1.0f;
     
     Mat2& operator *=(Mat2 const& m);
+    float det() const;
+
 };
 Mat2 operator*(Mat2 const& m1, Mat2 const& m2);
+Mat2 inverse(Mat2 const& m);
+Mat2 transpose(Mat2 const& m);
+Mat2 make_rotation_mat2(float phi);
 
 #endif
